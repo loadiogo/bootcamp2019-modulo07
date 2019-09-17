0b091cbef59358ef4ae1be10b66170050816f1a8
Diogo Silva <diogosilva@MacBook-Air-de-Diogo.local>
2019-09-16 22:24:41 -0300

    [Feature] add home component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/26/D13-2253-026/D13-2253-026_detalhe1.jpg?ims=280x280"
+          alt="Tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
